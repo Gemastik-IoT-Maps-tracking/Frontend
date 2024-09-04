b5a1f26fd66c65631a75a61d50741eedf976f03c
Naufal Irawan <irawanaufal29209@gmail.com>
2024-09-04 17:42:08 +0700

    Adding pak mara to biodata

--- src/components/about/Biodata.jsx
+++ src/components/about/Biodata.jsx
@@ -7,11 +7,13 @@ import {
   Typography,
   Tooltip,
 } from "@material-tailwind/react";
+import foto0 from "/src/assets/img/pak_mara.png";
 import foto1 from "/src/assets/img/naufal.png";
 import foto2 from "/src/assets/img/ghiyats.png";
 import foto3 from "/src/assets/img/hauzan.png";
 import linkedInIcon from "/src/assets/linkedin.svg";
 import githubIcon from "/src/assets/github.svg";
+import gunadarmaIcon from "/src/assets/gunadarma.svg";
 
 const linkedInLinks = [
   "https://www.linkedin.com/in/irawanaufal29",
@@ -25,13 +27,45 @@ const githubLinks = [
   "https://github.com/Shacent"
 ];
 
+const gstaffLinks = [
+  "http://maranugraha.staff.gunadarma.ac.id/",
+];
+
 export function ProfileCard() {
   return (
     <div className="bg-white rounded-lg shadow-lg p-6">
       <Typography variant="h3" color="blue-gray" className="text-center mb-6 font-bold">
-        Anggota Tim
+        Komposisi Tim
       </Typography>
       <div className="flex flex-wrap justify-center gap-4">
+        {/* ProfileCard 0 */}
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/5 p-2">
+          <Card className="w-full shadow-md hover:shadow-lg transition-shadow duration-300">
+            <CardHeader floated={false} className="h-48 rounded-t-lg overflow-hidden">
+              <img src={foto0} alt="Mara Nugraha" className="object-cover w-full h-full" />
+            </CardHeader>
+            <CardBody className="text-center p-3">
+              <Typography variant="h5" color="blue-gray" className="mb-2 font-semibold">
+                Dr. Mara Nugraha, SKom., MMSI
+              </Typography>
+              <Typography color="blue-gray" className="font-medium">
+                Dosen Pendamping
+              </Typography>
+            </CardBody>
+            <CardFooter className="flex justify-center gap-5 pt-2">
+              <Tooltip content="Staffsite Gunadarma">
+                <a
+                  href={gstaffLinks[0]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={gunadarmaIcon} alt="Gunadarma" className="w-5 h-5" />
+                </a>
+              </Tooltip>
+            </CardFooter>
+          </Card>
+        </div>
+
         {/* ProfileCard 1 */}
         <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/5 p-2">
           <Card className="w-full shadow-md hover:shadow-lg transition-shadow duration-300">
@@ -40,8 +74,7 @@ export function ProfileCard() {
             </CardHeader>
             <CardBody className="text-center p-3">
               <Typography variant="h5" color="blue-gray" className="mb-2 font-semibold">
-                Naufal Maulana
-                <br />Al-Ghifari Irawan
+                Naufal Maulana Al-Ghifari Irawan
               </Typography>
               <Typography color="blue-gray" className="font-medium">
                 Ketua
@@ -78,8 +111,7 @@ export function ProfileCard() {
             </CardHeader>
             <CardBody className="text-center p-3">
               <Typography variant="h5" color="blue-gray" className="mb-2 font-semibold">
-                Ghiyats Ibnu
-                <br />Syahied
+                Ghiyats Ibnu Syahied
               </Typography>
               <Typography color="blue-gray" className="font-medium">
                 Anggota
@@ -116,8 +148,7 @@ export function ProfileCard() {
             </CardHeader>
             <CardBody className="text-center p-3">
               <Typography variant="h5" color="blue-gray" className="mb-2 font-semibold">
-                Muhammad Hauzan
-                <br />Dini Fakhri
+                Muhammad Hauzan Dini Fakhri
               </Typography>
               <Typography color="blue-gray" className="font-medium">
                 Anggota
